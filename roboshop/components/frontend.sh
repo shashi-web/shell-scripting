0dca4e269316ae51fef295e93a0ae72186cc97b5
shashidhar <raoshashidhar.3@gmail.com>
2021-02-14 02:49:45 -0500

    maven-shell

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -3,22 +3,27 @@ COMPONENT=frontend
 source components/common.sh
 
 maven "Installing nginx" "yum install nginx -y"
+yum install nginx -y
 stat $?
-#yum install nginx -y
 maven "starting nginx service" "systemctl start nginx"
-stat $?
-exit
-
 systemctl enable nginx
 systemctl start nginx
-
+stat $?
+maven "download the HTDOCS content" "curl -s -L -o /tmp/frontend.zip "https://dev.azure.com/DevOps-Batches/e869c1fe-489d-4f58-b676-fb75fe4e9e12/_apis/git/repositories/18fbb315-ed99-4a65-9b79-e7eec2f3e1b1/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
+"
 curl -s -L -o /tmp/frontend.zip "https://dev.azure.com/DevOps-Batches/e869c1fe-489d-4f58-b676-fb75fe4e9e12/_apis/git/repositories/18fbb315-ed99-4a65-9b79-e7eec2f3e1b1/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
-
+stat $?
+maven "Deploy in Nginx Default Location" "cd /usr/share/nginx/html rm -rf *
+u ip /tmp/frontend.zip mv static/* . rm -rf static README.md"
 cd /usr/share/nginx/html
 rm -rf *
 unzip /tmp/frontend.zip
 mv static/* .
 rm -rf static README.md
+stat $?
+maven "deploy in roboshop.conf" "mv localhost.conf /etc/nginx/default.d/roboshop.conf
+"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
-
+stat $?
+maven "restart nginx" "systemctl restart nginx"
 systemctl restart nginx

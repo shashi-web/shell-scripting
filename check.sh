d9f5f73d85b6ae0cfe7bb8f651a30f42dedb2bd7
shashidhar <raoshashidhar.3@gmail.com>
2021-03-05 19:13:09 -0500

    mysql.sh

--- check.sh
+++ check.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 read_line() {
+  FILE=/Users/shashidharrao/Desktop/vishvesh/check.txt
   # CHECK IF IT HAS THIS STRING
-  if grep -q siteActive=true "$vishvesh"; then
+  if grep -q siteActive=true "$FILE"; then
   Stat $? # SomeString was found
   fi
 }

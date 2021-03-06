9c2c4f65a375d861fff304b17aee0c6571bb6ddf
shashidhar <raoshashidhar.3@gmail.com>
2021-03-05 19:10:07 -0500

    mysql.sh

--- check.sh
+++ check.sh
@@ -2,12 +2,7 @@
 
 read_line() {
   # CHECK IF IT HAS THIS STRING
-  FILE="/Users/shashidharrao/Desktop/vishvesh/test.txt"
-  STRING="siteActive=true"
-  EXIT=1
-   while [ $EXIT -ne 0 ]; do
-    if [ -f $FILE ] ; then
-      CHECK IF THE "STRING" IS IN THE FILE - IF YES echo "FOUND"; EXIT=0;
-    fi
-   done
+  if grep -q siteActive=true "$vishvesh"; then
+  Stat $? # SomeString was found
+  fi
 }

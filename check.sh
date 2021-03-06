3cb770fe02f8ab3f03ef15808e3ab9c8f960e34e
shashidhar <raoshashidhar.3@gmail.com>
2021-03-05 19:25:49 -0500

    mysql.sh

--- check.sh
+++ check.sh
@@ -2,9 +2,9 @@
 
 read_line() {
   # shellcheck disable=SC2125
-  FILE=/Users/shashidharrao/Desktop/vishvesh/*
+  FILE=/Users/shashidharrao/Desktop/vishvesh
   # CHECK IF IT HAS THIS STRING
-  if grep -q siteActive=true "$FILE"; then
+  if grep -R siteActive=true "$FILE"; then
   echo "found" # SomeString was found
   else
     echo "not found"
